21fb5e33a59ac038f7dae2de78cf2df09003b9bc
Den Titkov <d.g.titkov@gmail.com>
2023-02-06 12:33:43 +0300

    added sEd6

---
+++ sEd6/task41_/Program.cs
@@ -0,0 +1,16 @@
+﻿//Task 41: The user enters M numbers from the keyboard.
+// Count how many numbers greater than 0 the user entered.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Enter the number of values you want to enter: ");
+int numberOfValues = int.Parse(Console.ReadLine());
+int count = 0;
+
+for (int i = 0; i < numberOfValues; i++)
+{
+    Console.Write("Enter a number: ");
+    int number = int.Parse(Console.ReadLine());
+    if (number > 0) count++;
+}
+Console.WriteLine("->" + count);
---
+++ sEd6/task43_contrLines/Program.cs
@@ -0,0 +1,20 @@
+﻿// Task 43: Write a program to find the intersection point
+// two straight lines given by equations y = k1 * x + b1, y = k2 * x + b2; 
+// the values ​​b1, k1, b2 and k2 are set by the user.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int PromptNumber(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+double pointB1 = PromptNumber("Enter value b1: ");
+double pointK1 = PromptNumber("Enter value k1: ");
+double pointB2 = PromptNumber("Enter value b2: ");
+double pointK2 = PromptNumber("Enter value k2: ");
+
+double x=(pointB2-pointB1)/(pointK1-pointK2);
+double y=(pointK1*(pointB2-pointB1))/(pointK1-pointK2)+pointB1;
+
+Console.Write($"b1 = {pointB1}, k1 = {pointK1}, b2 = {pointB2}, k2 = {pointK2} -> ({x}; {y})");
